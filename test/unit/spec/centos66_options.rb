b8a59afce02841d5512b4fbfe192e9f7b8e61d78
Kostas Georgakopoulos <kostas.georgakopoulos@rackspace.co.uk>
2015-08-11 10:23:28 +0100

    Fix for issue #8

--- test/unit/spec/centos66_options.rb
+++ test/unit/spec/centos66_options.rb
@@ -1,5 +1,5 @@
- CENTOS66_SERVICE_OPTS = {
-   log_level: LOG_LEVEL,
-   platform: 'centos',
-   version: '6.6'
- }
+CENTOS66_SERVICE_OPTS = {
+  log_level: LOG_LEVEL,
+  platform: 'centos',
+  version: '6.6'
+}
--- test/unit/spec/ubuntu1404_options.rb
+++ test/unit/spec/ubuntu1404_options.rb
@@ -1,5 +1,5 @@
-  UBUNTU1404_SERVICE_OPTS = {
-    log_level: LOG_LEVEL,
-    platform: 'ubuntu',
-    version: '14.04'
-  }
+UBUNTU1404_SERVICE_OPTS = {
+  log_level: LOG_LEVEL,
+  platform: 'ubuntu',
+  version: '14.04'
+}
